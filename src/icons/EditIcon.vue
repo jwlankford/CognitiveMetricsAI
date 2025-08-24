a62d9a2db9a95f4f2c1d4911bff1d1ffb0f8db68
Jeremy Lankford <jwlankford@gmail.com>
2025-08-24 13:22:22 -0400

    push to run

---
+++ src/icons/EditIcon.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536M9 13l6.586-6.586a2 2 0 112.828 2.828L11.828 15.828a2 2 0 01-1.414.586H7v-3a2 2 0 01.586-1.414z" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+// No script needed
+</script>
---
+++ src/icons/SaveIcon.vue
@@ -0,0 +1,10 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75A2.25 2.25 0 014.5 4.5h3.379c.414 0 .81.172 1.1.477l1.272 1.346c.29.305.686.477 1.1.477H19.5a2.25 2.25 0 012.25 2.25v8.25a2.25 2.25 0 01-2.25 2.25H4.5A2.25 2.25 0 012.25 17.25V6.75z" />
+    <path stroke-linecap="round" stroke-linejoin="round" d="M12 11.25v3.75m0 0l-1.5-1.5m1.5 1.5l1.5-1.5" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+// Folder arrow down icon (Heroicons)
+</script>
